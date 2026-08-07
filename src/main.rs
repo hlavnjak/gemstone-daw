d4f65a6771fc062d48043014dbe17d8ff3eb4a04
Jakub Hlavnicka <hlavnickajakub@gmail.com>
2026-08-07 21:29:12 +0200

    Bump the copyright year in every source header to 2026

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-// Copyright 2025 Jakub Hlavnicka
+// Copyright 2026 Jakub Hlavnicka
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
